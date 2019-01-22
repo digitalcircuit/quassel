f3d6b044e9c726037a54ccb3b70dd471323ea63a
Shane Synan <digitalcircuit36939@gmail.com>
2019-01-21 23:10:50 -0500

    core: Create plpgsql for compat with PSQL < 9.0
    Add calls to create/replace "plpgsql" language during function
    creation.  PostgreSQL versions earlier than 9.0 do not load the
    "plpgsql" language by default.  If the language is already loaded,
    the call should do nothing.
    
    Handle this during upgrades and database creation, ensuring that no
    matter when the database is created, it will have "plpgsql" loaded.
    
    No new schema version added as existing databases must have loaded
    "plpgsql" in order to successfully upgrade.  By that point, "plpgsql"
    should be persisted in the database.
    
    Unfortunately, "CREATE OR REPLACE" only works on PostgreSQL 9.0+, so
    we have to employ an ugly workaround, as written by Tim Murphy.
    
    See https://timmurphy.org/2011/08/27/create-language-if-it-doesnt-exist-in-postgresql/
    And https://www.postgresql.org/docs/current/sql-createlanguage.html
    And https://www.postgresql.org/docs/current/plpgsql-overview.html

---
+++ src/core/SQL/PostgreSQL/version/21/upgrade_000_create_psql_lang.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION create_language_plpgsql()
+RETURNS BOOLEAN AS $$
+    CREATE LANGUAGE plpgsql;
+    SELECT TRUE;
+$$ LANGUAGE SQL;
+
+SELECT CASE WHEN NOT
+    (
+        SELECT  TRUE AS exists
+        FROM    pg_language
+        WHERE   lanname = 'plpgsql'
+        UNION
+        SELECT  FALSE AS exists
+        ORDER BY exists DESC
+        LIMIT 1
+    )
+THEN
+    create_language_plpgsql()
+ELSE
+    FALSE
+END AS plpgsql_created;
+
+DROP FUNCTION create_language_plpgsql();
+-- Thanks to https://timmurphy.org/2011/08/27/create-language-if-it-doesnt-exist-in-postgresql/
+
+--CREATE OR REPLACE LANGUAGE plpgsql; -- For PostgreSQL < 9.0 compatibility
